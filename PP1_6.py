24c6d299df10ce743c48491f9240d75c0f732034
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-24 13:58:50 +0000

    first commit

--- PP1_6.py
+++ PP1_6.py
@@ -1,8 +1,8 @@
 '''
-	File Name: errors.py
-	Author: Mr. Kalisz
-	Date Created: March 29, 2019
-	Date Last Edited: Sept 23, 2024
+	File Name: Error Checking
+	Author: Jonisa Shanmuganantham
+	Date Created: Sept 24, 2024
+	Date Last Edited: Sept 24, 2024
 '''
 
 # From input, recieve two integers from the user and add them together.  Output the result.
@@ -10,7 +10,7 @@ def q1():
   num1 = input("Input a number: ")
   num2 = input("Input a number: ")
   
-  Print(num + num2)
+  Print(num1 + num2)
 
 # From input recieve two integers.  Output the quotient rounded down.
 
@@ -18,21 +18,20 @@ def q2():
   num1 = input("Input a number: ")
   num2 = input("Input a number: ")
   
-  print(num1/num2)
+  print(num1//num2)
 
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print (hello Mr. Kalisz have you seen my work yet?)
+  print ("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
 
 def q4():
-  input("Input a number: ")
+  num1 = input("Input a number: ")
   num2 = input("Input a number: ")
-  
-  print (int(num1 * num2))
+  print(int(num1 * num2))
 
 q1()
 q2()
